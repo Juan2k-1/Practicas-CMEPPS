2864296c9b40fdfd6495aba5a57e12fc36e39927
Juan2k-1 <jadova16@gmail.com>
2023-10-10 10:17:26 +0200

    Añadida la sesion tutorial de TTD en su primera version

---
+++ Practica3_Tutorial_TDD/src/modelo/Calculadora.java
@@ -0,0 +1,15 @@
+package modelo;
+
+public class Calculadora {
+
+	public static Integer suma(int i, int j) {
+
+		return null;
+	}
+
+	public static Integer resta(int i, int j) {
+
+		return null;
+	}
+
+}
