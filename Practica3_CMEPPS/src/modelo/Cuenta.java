992969d1b8ff42496efcd5e5886e538ea0391607
Juan2k-1 <jadova16@gmail.com>
2023-10-10 10:54:27 +0200

    Añadido los pasos 1 y 2 de TDD, codigo minimo para comprobar que funcione el test ingresar

---
+++ Practica3_CMEPPS/src/modelo/Cuenta.java
@@ -0,0 +1,20 @@
+package modelo;
+
+public class Cuenta {
+	
+	private double saldo;
+
+	public Cuenta(String numeroCuenta, String titular, double saldo) {
+		super();
+		this.saldo = saldo;
+	}
+
+	public void ingresar(double monto) {
+		
+	}
+
+	public double getSaldo() {
+		return 1000;
+	}
+
+}
---
+++ Practica3_CMEPPS/src/test/CuentaTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import modelo.Cuenta;
+
+class CuentaTest {
+
+	static Cuenta cuenta;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		cuenta = new Cuenta("12345", "Manuel", 0);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIngresar() {
+		cuenta.ingresar(1000);
+		assertEquals(1000, cuenta.getSaldo());
+	}
+
+	@Test
+	void testRetirar() {
+
+	}
+
+}
