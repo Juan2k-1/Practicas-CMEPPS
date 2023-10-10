565a59c7bf9f6d2537c1d059886521107634203b
Juan2k-1 <jadova16@gmail.com>
2023-10-10 18:44:34 +0200

    Añadido el test0014 para comprobar el correcto funcionamiento de las clases

--- Practica3_CMEPPS/src/modelo/Cuenta.java
+++ Practica3_CMEPPS/src/modelo/Cuenta.java
@@ -1,12 +1,21 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cuenta {
 
 	private double saldo;
+	private String numero;
+	private String titular;
+	private List<Movimiento> mMovimientos;
 
 	public Cuenta(String numeroCuenta, String titular, double saldo) {
 		super();
 		this.saldo = saldo;
+		this.setNumero(numeroCuenta);
+		this.setTitular(titular);
+		this.mMovimientos = new ArrayList<Movimiento>();
 	}
 
 	public void ingresar(double monto) {
@@ -14,7 +23,9 @@ public class Cuenta {
 	}
 
 	public void retirar(double monto) {
-		this.setSaldo(this.getSaldo() - monto);
+		if (this.getSaldo() >= -500) {
+			this.setSaldo(this.getSaldo() - monto);
+		} 
 	}
 
 	public void setSaldo(double saldo) {
@@ -23,5 +34,29 @@ public class Cuenta {
 
 	public double getSaldo() {
 		return this.saldo;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public List<Movimiento> getmMovimientos() {
+		return mMovimientos;
+	}
+
+	public void setmMovimientos(List<Movimiento> mMovimientos) {
+		this.mMovimientos = mMovimientos;
 	}
 }
--- Practica3_CMEPPS/src/test/CuentaTest.java
+++ Practica3_CMEPPS/src/test/CuentaTest.java
@@ -2,6 +2,8 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,14 +11,18 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import modelo.Cuenta;
+import modelo.Movimiento;
+import modelo.Movimiento.Signo;
 
 class CuentaTest {
 
 	static Cuenta cuenta;
+	static Cuenta cuenta2;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		cuenta = new Cuenta("12345", "Manuel", 0);		
+		cuenta = new Cuenta("12345", "Manuel", 50);
+		cuenta2 = new Cuenta("67890", "Juan", 0);
 	}
 
 	@AfterAll
@@ -25,7 +31,8 @@ class CuentaTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
-		cuenta.setSaldo(0);
+		cuenta.setSaldo(50);
+		cuenta2.setSaldo(0);
 	}
 
 	@AfterEach
@@ -34,13 +41,72 @@ class CuentaTest {
 
 	@Test
 	void testIngresar() {
-		cuenta.ingresar(1000);
-		assertEquals(1000, cuenta.getSaldo());
+
+		// assertEquals(1000, cuenta.getSaldo());
 	}
 
 	@Test
 	void testRetirar() {
-		cuenta.retirar(1000);
-		assertEquals(-1000, cuenta.getSaldo());
+
+		// assertEquals(-1000, cuenta.getSaldo());
+	}
+
+	@Test
+	void test0014() {
+
+		// cuenta.ingresar(100);
+		// cuenta.retirar(200);
+		// cuenta.retirar(200);
+
+		/*
+		 * double saldoActual = cuenta.getSaldo(); List<Movimiento> movimientos =
+		 * cuenta.getmMovimientos(); for (int i = 0; i <
+		 * cuenta.getmMovimientos().size(); i++) { Movimiento movimiento =
+		 * movimientos.get(i); if (movimiento.getSigno() == Signo.D) { saldoActual -=
+		 * movimiento.getImporte(); System.out.println("Retirada de " +
+		 * movimientos.get(i).getImporte() + "€"); } else if (movimiento.getSigno() ==
+		 * Signo.H) { saldoActual += movimiento.getImporte();
+		 * System.out.println("Ingreso de " + movimientos.get(i).getImporte() + "€"); }
+		 * 
+		 * if (saldoActual < -500) { System.out.println("Fondos insuficientes " + "(" +
+		 * saldoActual + "€" + ")" + " en la cuenta " + cuenta.getNumero() +
+		 * "para la retirada de " + movimientos.get(i).getImporte() + "€"); } }
+		 */
+
+		// assertEquals(-250, cuenta.getSaldo());
+
+		List<Movimiento> movimientos2 = cuenta2.getmMovimientos();
+		double saldoActual2 = cuenta2.getSaldo();
+		System.out.println("Saldo inicial: " + saldoActual2);
+
+		Movimiento movimiento1 = new Movimiento(350, "Retirada", Signo.D);
+		Movimiento movimiento2 = new Movimiento(200, "Retirada", Signo.D);
+		Movimiento movimiento3 = new Movimiento(150, "Retirada", Signo.D);
+		Movimiento movimiento4 = new Movimiento(50, "Ingreso", Signo.H);
+		Movimiento movimiento5 = new Movimiento(100, "Retirada", Signo.D);
+		movimientos2.add(movimiento1);
+		movimientos2.add(movimiento2);
+		movimientos2.add(movimiento3);
+		movimientos2.add(movimiento4);
+		movimientos2.add(movimiento5);
+
+		for (int i = 0; i < cuenta2.getmMovimientos().size(); i++) {
+			Movimiento movimiento_aux = movimientos2.get(i);
+			if (movimiento_aux.getSigno() == Signo.D && saldoActual2 - movimiento_aux.getImporte() >= -500) {
+				saldoActual2 -= movimiento_aux.getImporte();
+				cuenta2.retirar(movimiento_aux.getImporte());
+				System.out.println("Retirada de " + movimientos2.get(i).getImporte() + "€");
+			} else if (movimiento_aux.getSigno() == Signo.H) {
+				saldoActual2 += movimiento_aux.getImporte();
+				 cuenta2.ingresar(movimiento_aux.getImporte());
+				System.out.println("Ingreso de " + movimientos2.get(i).getImporte() + "€");
+			} else if (saldoActual2 - movimiento_aux.getImporte() < -500) {
+				System.out.println("Fondos insuficientes " + "(" + saldoActual2 + "€" + ")" + " en la cuenta "
+						+ cuenta2.getNumero() + " para la retirada de " + movimientos2.get(i).getImporte() + "€");
+			}
+		}
+
+		//assertEquals(-450, saldoActual2);
+		assertEquals(saldoActual2, cuenta2.getSaldo());
 	}
 }
